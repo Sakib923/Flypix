376f93d58d5226646be8251c7d6558564a36e046
Faraby <sakibulalam923@gmail.com>
2026-09-03 23:50:05 +0600

    feat(layout): add ScrollRestoration to MainLayout component

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,15 +1,16 @@
-import Footer from "@/components/Footer.jsx";
-import Navbar from "../components/Navbar.jsx";
-import {Outlet} from "react-router";
-import TailwindDev from "../test/TailwindDev.jsx"
+import Footer from '@/components/Footer.jsx';
+import Navbar from '../components/Navbar.jsx';
+import { Outlet, ScrollRestoration } from 'react-router';
+import TailwindDev from '../test/TailwindDev.jsx';
 
 export default function MainLayout() {
     return (
         <div>
             <Navbar />
             <TailwindDev />
+            <ScrollRestoration />
             <Outlet />
             <Footer />
         </div>
-    )
+    );
 }
