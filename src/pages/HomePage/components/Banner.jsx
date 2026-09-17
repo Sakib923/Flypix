1c6cd7e6eac87fc2acdb23118e563aea76f9d020
Faraby <sakibulalam923@gmail.com>
2026-09-17 23:11:26 +0600

    refactor(Banner): align utility classes with Tailwind naming convention

--- src/pages/HomePage/components/Banner.jsx
+++ src/pages/HomePage/components/Banner.jsx
@@ -1,7 +1,7 @@
 export default function Banner() {
     return (
-        <div className="sm:px-[0px] md:px-[24px] lg:px-[52px] xl:px-[80px] 2xl:px-[80px]">
-            <div className="rounded-0 lg:gap-auto mt-7.5 flex h-fit flex-col items-center justify-between gap-y-[32px] bg-black px-9.25 py-10.5 md:flex-row md:gap-[35px] md:rounded-3xl lg:mx-[30px]">
+        <div className="sm:px-0 md:px-6 lg:px-13 xl:px-20 2xl:px-20">
+            <div className="rounded-0 lg:gap-auto mt-7.5 flex h-fit flex-col items-center justify-between gap-y-8 bg-black px-9.25 py-10.5 md:flex-row md:gap-8.75 md:rounded-3xl lg:mx-7.5">
                 <div className="text-center text-white">
                     <h2 className="text-[12px] font-medium text-[#77B526] sm:text-[12px] md:text-[14px] lg:text-[20px] xl:text-[26px]">
                         FLY BEYOND LIMITS
@@ -16,7 +16,7 @@ export default function Banner() {
                 </div>
                 <img
                     src="src/assets/images/Drone.png"
-                    className="aspect-auto h-auto w-[359px] object-contain sm:w-[359px] md:w-[358.826px] lg:w-[500px] xl:w-[630px]"
+                    className="aspect-auto h-auto w-89.75 object-contain sm:w-89.75 md:w-[358.826px] lg:w-125 xl:w-157.5"
                     alt={'Drone Image'}
                 />
             </div>
