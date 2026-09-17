6d96a03cbd91792344ce89053a638714cc1d711c
Faraby <sakibulalam923@gmail.com>
2026-09-17 22:55:17 +0600

    refactor(MostSelling): improve layout consistency and update styling

--- src/pages/HomePage/components/MostSelling.jsx
+++ src/pages/HomePage/components/MostSelling.jsx
@@ -2,14 +2,14 @@ import Heading from '@/components/Heading';
 import Label from '@/components/Label';
 import FireIcon from '../../../assets/fire.svg?react';
 import MostSellingItemCard from '@/components/MostSellingItemCard';
-import DroneImage from "../../../assets/images/Drone.png";
+import DroneImage from '../../../assets/images/Drone.png';
 
 export default function MostSelling() {
     // Here we have to fetch some most selling drone and then map through them with MostSellingCard component
 
     return (
-        <div className="flex flex-col items-start gap-[24px]  py-[26px] bg-[#F8FAFC]">
-            <div className="flex flex-col items-center px-[16px] justify-center gap-[16px] self-stretch">
+        <div className="flex flex-col items-start gap-6 bg-[#F8FAFC] py-6.5">
+            <div className="flex flex-col items-center justify-center gap-4 self-stretch px-4 sm:px-7 md:px-10 lg:px-15 xl:px-20 2xl:px-20">
                 <Label label="Most Selling">
                     {' '}
                     <FireIcon />{' '}
@@ -18,10 +18,22 @@ export default function MostSelling() {
                     Grab the Best Deal Offered Specially for You
                 </Heading>
             </div>
-            <div className='flex gap-[20px] px-[16px] items-start self-stretch overflow-scroll scrollbar-none'>
-                <MostSellingItemCard name="DJI mavic mini" price="550" image={DroneImage} />
-                <MostSellingItemCard name="DJI mavic mini" price="550" image={DroneImage} />
-                <MostSellingItemCard name="DJI mavic mini" price="550" image={DroneImage} />
+            <div className="flex w-full scrollbar-none items-start gap-5 self-stretch overflow-scroll px-4 sm:px-7 md:px-10 lg:px-15 xl:px-20 2xl:px-20">
+                <MostSellingItemCard
+                    name="DJI mavic mini"
+                    price="550"
+                    image={DroneImage}
+                />
+                <MostSellingItemCard
+                    name="DJI mavic mini"
+                    price="550"
+                    image={DroneImage}
+                />
+                <MostSellingItemCard
+                    name="DJI mavic mini"
+                    price="550"
+                    image={DroneImage}
+                />
             </div>
         </div>
     );
